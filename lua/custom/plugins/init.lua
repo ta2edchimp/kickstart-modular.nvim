9d554078089af7fa91e946172ae74c9b1439a0d6
ta2edchimp <kontakt@andreaswindt.de>
2025-09-17 22:19:13 +0200

    Basic customizations

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,27 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+-- Make line numbers default
+vim.opt.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+vim.opt.relativenumber = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { eol = '', tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Hilight column 80 with a certain color
+vim.opt.colorcolumn = '80'
+
+-- Extracted from `lualine.nvim/lua/lualine/components/diagnostics/config`
+local signs = { Error = '󰅚 ', Warn = '󰀪 ', Info = '󰋽 ', Hint = '󰌶 ' }
+for type, icon in pairs(signs) do
+  local hl = 'DiagnosticSign' .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
 return {}
