0c475a708eae9c9d3d10fc341f5167f001cfae37
ta2edchimp <kontakt@andreaswindt.de>
2025-12-03 17:14:50 +0100

    vim.diagnostic: set WARN as minimum displayed diagnostics info

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -178,7 +178,7 @@ return {
       vim.diagnostic.config {
         severity_sort = true,
         float = { border = 'rounded', source = 'if_many' },
-        underline = { severity = vim.diagnostic.severity.ERROR },
+        underline = { severity = { min = vim.diagnostic.severity.WARN } },
         signs = vim.g.have_nerd_font and {
           text = {
             [vim.diagnostic.severity.ERROR] = '󰅚 ',
